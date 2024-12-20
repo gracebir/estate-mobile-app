ae4779010adefcfd7fa54d5b40d7849bf87e6657
gracebir <grace.devolop@gmail.com>
2024-12-20 23:01:08 +0200

    sign up screen

--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -1,16 +1,50 @@
 /** @format */
 
-import { StyleSheet, Text, View } from "react-native";
+import { Image, ScrollView, Text, TouchableOpacity, View } from "react-native";
 import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import images from "@/constants/images";
+import icons from "@/constants/icons";
 
 const Signin = () => {
     return (
-        <View>
-            <Text>Signin</Text>
-        </View>
+        <SafeAreaView className='h-full bg-white'>
+            <ScrollView contentContainerClassName='h-full'>
+                <Image
+                    source={images.onboarding}
+                    className='w-full h-4/6'
+                    resizeMode='contain'
+                />
+                <View className='px-10'>
+                    <Text className='text-base text-center uppercase font-rubik text-black-200'>
+                        Welcome to ReState
+                    </Text>
+                    <Text className='text-3xl font-rubik-bold text-black-300 text-center mt-2'>
+                        Let's Get You CLoser {"\n"}to{" "}
+                        <Text className='text-primary-300'>
+                            Your Ideal Home
+                        </Text>
+                    </Text>
+                    <Text className='text-base text-center mt-4 font-rubik text-black-100'>
+                        Login to Real Scout with Google
+                    </Text>
+
+                    <TouchableOpacity className='bg-white shadow-md  shadow-zinc-300 rounded-full w-full py-4 mt-5'>
+                        <View className='flex flex-row items-center gap-2 w-full border border-white justify-center'>
+                            <Image
+                                source={icons.google}
+                                className='w-5 h-5'
+                                resizeMode='contain'
+                            />
+                            <Text className='text-black-300 font-rubik-medium'>
+                                Sign Up with Google
+                            </Text>
+                        </View>
+                    </TouchableOpacity>
+                </View>
+            </ScrollView>
+        </SafeAreaView>
     );
 };
 
 export default Signin;
-
-const styles = StyleSheet.create({});
